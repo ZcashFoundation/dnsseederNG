8e8a69d6df3f88132a21370eba59df76064f0e5a
DC <alchemydc@proton.me>
2026-01-14 16:03:42 -0700

    fix(style)

--- build.rs
+++ build.rs
@@ -3,7 +3,6 @@ use vergen_gitcl::{BuildBuilder, Emitter, GitclBuilder};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let build = BuildBuilder::all_build()?;
     let gitcl = GitclBuilder::all_git()?;
-    
     Emitter::default()
         .add_instructions(&build)?
         .add_instructions(&gitcl)?
